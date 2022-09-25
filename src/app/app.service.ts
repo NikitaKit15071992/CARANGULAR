d4865eb1a6e825f5941f5e977d23a45ad21f9de8
Никита Попов <nik-popov-92@mail.ru>
2022-09-25 21:03:25 +0300

    Angular

---
+++ src/app/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
+import { getLocaleDateFormat } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+
+  sendQuery(data:any) {
+    return this.http.post('https://testologia.site/intensive-price', data);
+  }
+
+  getData() {
+    return this.http.get('https://testologia.site/intensive-data');
+  }
+}
